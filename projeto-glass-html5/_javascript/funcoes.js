cc745a7db93bd9b2e8dd01fbe257e642a88be2f9
Sandy-sayuri <sandysayuri200@gmail.com>
2020-07-19 21:37:25 -0300

    o menu está feito

---
+++ projeto-glass-html5/_javascript/funcoes.js
@@ -0,0 +1,21 @@
+function mudarfoto(foto){
+    if(foto==1){
+        document.getElementById('icone').src="_imagens/home.png";
+
+    }
+     else if(foto==2){
+        document.getElementById('icone').src="_imagens/especificacoes.png"
+    }
+    else if(foto==3){
+        document.getElementById('icone').src="_imagens/fotos.png"
+    }
+    else if(foto==4){
+        document.getElementById('icone').src="_imagens/multimidia.png"
+    }
+    else if(foto==5){
+        document.getElementById('icone').src="_imagens/contato.png"
+    }
+    else if(6){
+        document.getElementById("icone").src="_imagens/glass-oculos-preto-peq.png"
+    }
+}
